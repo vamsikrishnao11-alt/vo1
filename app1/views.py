20f7ac46490982321a806a322167d7ca257150bb
kayamvamsikrishna <kayamvamsikrishna@gmail.com>
2024-11-27 13:33:09 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+#Return http response in string formate
+def vamsi(request):
+    return HttpResponse('AI')
